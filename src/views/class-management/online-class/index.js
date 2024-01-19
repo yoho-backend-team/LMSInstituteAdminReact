5d2bd6cc293441cc71b4a0423da610c8b087a6ee
Arunbalajiyoho <140370898+Arunbalajiyoho@users.noreply.github.com>
2024-01-19 17:54:13 +0530

    arunbalaji commit

--- src/views/class-management/online-class/index.js
+++ src/views/class-management/online-class/index.js
@@ -1,20 +1,16 @@
 // material-ui
-import { Typography } from '@mui/material';
+// import { Typography } from '@mui/material';
 
 // project imports
-import MainCard from 'components/cards/MainCard';
-
+import OnlineClassCard from 'features/class-management/online-classes/components/OnlineClassCard';
 // ==============================|| SAMPLE PAGE ||============================== //
 
-const OnlineClass = () => (
-  <MainCard title="Sample Card">
-    <Typography variant="body2">
-      Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut enif ad
-      minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in reprehended
-      in voltage veil esse colum doolie eu fujian bulla parian. Exceptive sin ocean cuspidate non president, sunk in culpa qui officiate
-      descent molls anim id est labours.
-    </Typography>
-  </MainCard>
-);
+const OnlineClass = () => {
+  return (
+    <>
+      <OnlineClassCard />
+    </>
+  );
+};
 
 export default OnlineClass;
