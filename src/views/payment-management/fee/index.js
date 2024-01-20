9ebbff236c338d8db2b0eda33d1c876dcb255d04
Mohammed Thasthakir <mohammed.mt392@gmail.com>
2024-01-20 15:13:39 +0530

    thasthakir commmit

--- src/views/payment-management/fee/index.js
+++ src/views/payment-management/fee/index.js
@@ -5,19 +5,51 @@
 // import MainCard from 'components/cards/MainCard';
 import { useState } from 'react';
 import CustomizedInput from 'features/course-management/add-course/components/CustomizedInput';
+import { Grid } from '@mui/material';
 
 // ==============================|| SAMPLE PAGE ||============================== //
 
 const Fee = () => {
   const [features, setFeatures] = useState([]);
   return (
-    <CustomizedInput
-      placeholder={'Add New Features'}
-      data={features}
-      setData={setFeatures}
-      cardTitle={'Course Features'}
-      buttonTitle={'Add Corse Feature'}
-    />
+    <Grid container spacing={2}>
+      <Grid item xs={12} sm={6}>
+        <CustomizedInput
+          placeholder={'Add New Features'}
+          data={features}
+          setData={setFeatures}
+          cardTitle={'Course Features'}
+          buttonTitle={'Add Corse Feature'}
+        />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <CustomizedInput
+          placeholder={'Add New Features'}
+          data={features}
+          setData={setFeatures}
+          cardTitle={'Course Features'}
+          buttonTitle={'Add Corse Feature'}
+        />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <CustomizedInput
+          placeholder={'Add New Features'}
+          data={features}
+          setData={setFeatures}
+          cardTitle={'Course Features'}
+          buttonTitle={'Add Corse Feature'}
+        />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <CustomizedInput
+          placeholder={'Add New Features'}
+          data={features}
+          setData={setFeatures}
+          cardTitle={'Course Features'}
+          buttonTitle={'Add Corse Feature'}
+        />
+      </Grid>
+    </Grid>
   );
 };
 
