6eb2e272b9bcf12a86aed8fbd1ad7cb38594c464
Mohammed Thasthakir <mohammed.mt392@gmail.com>
2024-01-22 15:25:47 +0530

    thatshakir commit

--- src/features/branch-management/branches/components/batchcard.js
+++ src/features/branch-management/branches/components/batchcard.js
@@ -1,156 +1,14 @@
-// import React from 'react';
-// import { Card, CardContent, Typography } from '@mui/material';
-// import CardMedia from '@mui/material/CardMedia';
-// import Grid from '@mui/material/Grid';
-
-// const BatchCard = () => {
-//   return (
-//     <Grid container spacing={3}>
-//       <Grid item xs={12} sm={6} md={3}>
-//         <Card sx={{ backgroundColor: '#e3e1fc' }}>
-
-//           <CardMedia
-//             sx={{
-//               height: '14.5625rem',
-//               width: '80%',
-//               display: 'flex',
-//               justifyContent: 'center', // Center the image horizontally
-//               alignItems: 'center', // Center the image vertically
-//               margin: 'auto' // Center the image horizontally within the card
-//             }}
-//             image={`https://cdn-icons-png.flaticon.com/512/992/992651.png`}
-//           />
-//           <CardContent>
-//             <Typography variant="h5" sx={{ mb: 2 }}>
-//               Location
-//             </Typography>
-//             <Typography sx={{ color: 'text.secondary' }}>
-//               29/1, Ambal Nagar, 1st Main Road, Echankadu, Kovilambakam, Chennai, TamilNadu- 600117
-//             </Typography>
-//           </CardContent>
-//         </Card>
-//       </Grid>
-
-//       <Grid item xs={12} sm={6} md={3}>
-//         <Card sx={{ backgroundColor: '#e3e1fc' }}>
-//           <CardMedia
-//             sx={{
-//               height: '14.5625rem',
-//               width: '80%',
-//               display: 'flex',
-//               justifyContent: 'center', // Center the image horizontally
-//               alignItems: 'center', // Center the image vertically
-//               margin: 'auto' // Center the image horizontally within the card
-//             }}
-//             image={`https://cdn3d.iconscout.com/3d/premium/thumb/company-building-7637349-6168858.png?f=webp`}
-//           />
-//           <CardContent>
-//             <Typography variant="h5" sx={{ mb: 2 }}>
-//               Location
-//             </Typography>
-//             <Typography sx={{ color: 'text.secondary' }}>
-//               29/1, Ambal Nagar, 1st Main Road, Echankadu, Kovilambakam, Chennai, TamilNadu- 600117
-//             </Typography>
-//           </CardContent>
-//         </Card>
-//       </Grid>
-
-//       <Grid item xs={12} sm={6} md={3}>
-//         <Card sx={{ backgroundColor: '#e3e1fc' }}>
-//           <CardMedia
-//             sx={{
-//               height: '14.5625rem',
-//               width: '80%',
-//               display: 'flex',
-//               justifyContent: 'center', // Center the image horizontally
-//               alignItems: 'center', // Center the image vertically
-//               margin: 'auto' // Center the image horizontally within the card
-//             }}
-//             image={`https://cdn3d.iconscout.com/3d/premium/thumb/company-building-7637349-6168858.png?f=webp`}
-//           />
-//           <CardContent>
-//             <Typography variant="h5" sx={{ mb: 2 }}>
-//               Location
-//             </Typography>
-//             <Typography sx={{ color: 'text.secondary' }}>
-//               29/1, Ambal Nagar, 1st Main Road, Echankadu, Kovilambakam, Chennai, TamilNadu- 600117
-//             </Typography>
-//           </CardContent>
-//         </Card>
-//       </Grid>
-
-//       <Grid item xs={12} sm={6} md={3}>
-//         <Card sx={{ backgroundColor: '#e3e1fc' }}>
-//           <CardMedia
-//             sx={{
-//               height: '14.5625rem',
-//               width: '80%',
-//               display: 'flex',
-//               justifyContent: 'center', // Center the image horizontally
-//               alignItems: 'center', // Center the image vertically
-//               margin: 'auto' // Center the image horizontally within the card
-//             }}
-//             image={`https://cdn3d.iconscout.com/3d/premium/thumb/company-building-7637349-6168858.png?f=webp`}
-//           />
-//           <CardContent>
-//             <Typography variant="h5" sx={{ mb: 2 }}>
-//               Location
-//             </Typography>
-//             <Typography sx={{ color: 'text.secondary' }}>
-//               29/1, Ambal Nagar, 1st Main Road, Echankadu, Kovilambakam, Chennai, TamilNadu- 600117
-//             </Typography>
-//           </CardContent>
-//         </Card>
-//       </Grid>
-//       <Grid item xs={12} sm={6} md={3}>
-//         <Card sx={{ backgroundColor: '#e3e1fc' }}>
-//           <CardMedia
-//             sx={{
-//               height: '14.5625rem',
-//               width: '80%',
-//               display: 'flex',
-//               justifyContent: 'center', // Center the image horizontally
-//               alignItems: 'center', // Center the image vertically
-//               margin: 'auto' // Center the image horizontally within the card
-//             }}
-//             image={`https://cdn3d.iconscout.com/3d/premium/thumb/company-building-7637349-6168858.png?f=webp`}
-//           />
-//           <CardContent>
-//             <Typography variant="h5" sx={{ mb: 2 }}>
-//               Location
-//             </Typography>
-//             <Typography sx={{ color: 'text.secondary' }}>
-//               29/1, Ambal Nagar, 1st Main Road, Echankadu, Kovilambakam, Chennai, TamilNadu- 600117
-//             </Typography>
-//           </CardContent>
-//         </Card>
-//       </Grid>
-//     </Grid>
-//   );
-// };
-
-// export default BatchCard;
-
-
-
-
-
-
-
-
-
-
-
-
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Card, CardContent, Typography, IconButton, Menu, MenuItem } from '@mui/material';
 import CardMedia from '@mui/material/CardMedia';
 import Grid from '@mui/material/Grid';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-
+import BranchesCardHeader from './BrachesCardHeader';
+// import { useTheme } from '@mui/system';
 const BatchCard = () => {
   const [anchorEl, setAnchorEl] = useState(null);
+  // const theme = useTheme();
 
   const handleMenuClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -176,53 +34,34 @@ const BatchCard = () => {
   };
 
   return (
-    <Grid container spacing={3}>
-      <Grid item xs={12} sm={6} md={3}>
-        <Card sx={{ backgroundColor: '#e3e1fc', position: 'relative' }}>
-          {/* <IconButton
-            aria-label="more"
-            aria-haspopup="true"
-            onClick={handleMenuClick}
-            sx={{
-              position: 'absolute',
-              top: 0,
-              right: 0,
-            }}
-          >
-            <MoreVertIcon />
-          </IconButton>
-
-          <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
-            <MenuItem onClick={handleViewClick}>View</MenuItem>
-            <MenuItem onClick={handleEditClick}>Edit</MenuItem>
-            <MenuItem onClick={handleDeleteClick}>Delete</MenuItem>
-          </Menu> */}
-
-          <CardMedia
-            sx={{
-              height: '14.5625rem',
-              width: '80%',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              margin: 'auto'
-            }}
-            image={`https://cdn-icons-png.flaticon.com/512/992/992651.png`}
-          />
-          <CardContent>
-            <Typography variant="h5" sx={{ mb: 2 }}>
-              Location
-            </Typography>
-            <Typography sx={{ color: 'text.secondary' }}>
-              29/1, Ambal Nagar, 1st Main Road, Echankadu, Kovilambakam, Chennai, TamilNadu- 600117
-            </Typography>
+    <Grid container spacing={2}>
+      {/* <Grid item xs={12} sm={6} md={3}>
+        <Card sx={{ position: 'relative', height: '13.5625rem' }}>
+          <CardContent sx={{ alignItems: 'center', justifyContent: 'center' }}>
+            <Avatar
+              sx={{
+                width: 100,
+                height: 100,
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                margin: 'auto'
+              }}
+              alt="jjj"
+              src={`https://cdn-icons-png.flaticon.com/512/992/992651.png`}
+            />
+            <Button sx={{ mx: 'auto', justifyContent: 'center', alignItems: 'center', display: 'flex',mt:3 }} variant="contained">
+              Add New Branch
+            </Button>
           </CardContent>
         </Card>
+      </Grid> */}
+      <Grid item xs={12}>
+        <BranchesCardHeader />
       </Grid>
-
       {Array.from({ length: 5 }, (_, index) => (
         <Grid item xs={12} sm={6} md={3} key={index}>
-          <Card sx={{ backgroundColor: '#e3e1fc', position: 'relative' }}>
+          <Card sx={{ position: 'relative' }}>
             <IconButton
               aria-label="more"
               aria-controls={`menu-${index}`}
@@ -230,8 +69,8 @@ const BatchCard = () => {
               onClick={handleMenuClick}
               sx={{
                 position: 'absolute',
-                top: 0,
-                right: 0
+                top: 5,
+                right: 3
               }}
             >
               <MoreVertIcon />
@@ -251,20 +90,21 @@ const BatchCard = () => {
 
             <CardMedia
               sx={{
-                height: '14.5625rem',
-                width: '80%',
+                height: 100,
+                width: 100,
                 display: 'flex',
                 justifyContent: 'center',
                 alignItems: 'center',
-                margin: 'auto'
+                mt: 3,
+                mx: 'auto'
               }}
-              image={`https://cdn3d.iconscout.com/3d/premium/thumb/company-building-7637349-6168858.png?f=webp`}
+              image={require('assets/images/avatar/map-pin.png')}
             />
             <CardContent>
-              <Typography variant="h5" sx={{ mb: 2 }}>
+              <Typography variant="h4" sx={{ mb: 2 }}>
                 Location
               </Typography>
-              <Typography sx={{ color: 'text.secondary' }}>
+              <Typography variant="h6" sx={{ color: 'text.secondary' }}>
                 29/1, Ambal Nagar, 1st Main Road, Echankadu, Kovilambakam, Chennai, TamilNadu- 600117
               </Typography>
             </CardContent>
