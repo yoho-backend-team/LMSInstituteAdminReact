7bd08022f2d4ca97ec34b9adc42ff75cf88a00fa
sanjayyoho <140371314+sanjayyoho@users.noreply.github.com>
2024-02-01 15:03:16 +0530

    sanjay commit

--- src/features/user-management/user-view/components/UserEditDialog.js
+++ src/features/user-management/user-view/components/UserEditDialog.js
@@ -1,32 +1,86 @@
-import React, { useState, useEffect } from 'react';
+import { Box, Button, Grid } from '@mui/material';
 import Dialog from '@mui/material/Dialog';
-import MenuItem from '@mui/material/MenuItem';
-import { styled } from '@mui/material/styles';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
 import DialogActions from '@mui/material/DialogActions';
-import { Grid, Box, Button } from '@mui/material';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
 import TextField from '@mui/material/TextField';
+import { styled } from '@mui/material/styles';
+import { useEffect, useState } from 'react';
+import MenuItem from '@mui/material/MenuItem';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Controller, useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { getAllActiveGroups } from '../services/viewUserServices';
 
-import { getAllActiveGroups, updateUser } from '../services/viewUserServices';
+const showErrors = (field, valueLen, min) => {
+  if (valueLen === 0) {
+    return `${field} field is required`;
+  } else if (valueLen > 0 && valueLen < min) {
+    return `${field} must be at least ${min} characters`;
+  } else {
+    return '';
+  }
+};
+const schema = yup.object().shape({
+  full_name: yup
+    .string()
+    .min(3, (obj) => showErrors('Full name', obj.value.length, obj.min))
+    .required(),
+  user_name: yup
+    .string()
+    .min(3, (obj) => showErrors('User name', obj.value.length, obj.min))
+    .required(),
+  email: yup.string().email().required(),
+  status: yup.string().required(),
+  contact: yup
+    .number()
+    .typeError('Contact Number field is required')
+    .min(10, (obj) => showErrors('Contact Number', obj.value.length, obj.min))
+    .required(),
+  designation: yup.string().required(),
+  role: yup.number().required()
+});
 
-const UserEditDialog = ({ id, userData, openEdit, handleEditClose }) => {
+const defaultValues = {
+  full_name: '',
+  user_name: '',
+  status: '',
+  email: '',
+  contact: Number(''),
+  designation: '',
+  role: ''
+};
+
+const UserEditDialog = ({ openEdit, handleEditClose }) => {
+  const {
+    reset,
+    control,
+    setValue,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    defaultValues,
+    mode: 'onChange',
+    resolver: yupResolver(schema)
+  });
+  const handleClose = () => {
+    setValue('full_name', '');
+    setValue('user_name', '');
+    setValue('status', '');
+    setValue('email', '');
+    setValue('contact', Number(''));
+    setValue('designation', '');
+    setValue('role', '');
+    handleEditClose();
+    reset();
+  };
+
   const image = require('assets/images/avatar/1.png');
   const [inputValue, setInputValue] = useState('');
   const [selectedImage, setSelectedImage] = useState('');
   const [imgSrc, setImgSrc] = useState(image);
   const [groups, setGroups] = useState([]);
-  const [formState, setFormState] = useState({
-    id: id,
-    name: '',
-    email: '',
-    phone: '',
-    employee_id: '',
-    designation: '',
-    is_active: '',
-    role: '',
-    username: ''
-  });
+  console.log(selectedImage);
   useEffect(() => {
     getAllGroups();
   }, []);
@@ -72,38 +126,6 @@ const UserEditDialog = ({ id, userData, openEdit, handleEditClose }) => {
     }
   };
 
-  const handleEdit = async () => {
-    var bodyFormData = new FormData();
-    bodyFormData.append('image', selectedImage);
-    bodyFormData.append('id', formState.id);
-    bodyFormData.append('name', formState.name);
-    bodyFormData.append('email', formState.email);
-    bodyFormData.append('mobile', formState.phone);
-    bodyFormData.append('designation', formState.designation);
-    bodyFormData.append('username', formState.username);
-    bodyFormData.append('role_id', formState?.role);
-    bodyFormData.append('is_active', formState?.is_active);
-
-    try {
-      const result = await updateUser(bodyFormData);
-      if (result.success) {
-        console.log('User Data:', result.data);
-        handleEditClose();
-      } else {
-        console.log(result.message);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormState((prevFormState) => ({
-      ...prevFormState,
-      [name]: value
-    }));
-  };
   return (
     <Dialog
       open={openEdit}
@@ -112,126 +134,200 @@ const UserEditDialog = ({ id, userData, openEdit, handleEditClose }) => {
       aria-describedby="user-view-edit-description"
       sx={{ '& .MuiPaper-root': { width: '100%', maxWidth: 750 } }}
     >
-      <DialogTitle
-        id="user-view-edit"
-        sx={{
-          textAlign: 'center',
-          fontSize: '1.5rem !important',
-          px: (theme) => [`${theme.spacing(5)} !important`, `${theme.spacing(10)} !important`],
-          pt: (theme) => [`${theme.spacing(6)} !important`, `${theme.spacing(5)} !important`]
-        }}
-      >
-        Edit User Information
-      </DialogTitle>
-      <DialogContent
-        sx={{
-          pb: (theme) => `${theme.spacing(3)} !important`,
-          px: (theme) => [`${theme.spacing(5)} !important`, `${theme.spacing(8)} !important`]
-        }}
-      >
-        <Grid container spacing={3}>
-          <Grid item xs={12} sm={12} sx={{ alignItems: 'center', justifyContent: 'center' }}>
-            <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-              <ImgStyled src={imgSrc} alt="Profile Pic" />
-              <div>
-                <ButtonStyled component="label" variant="contained" htmlFor="account-settings-upload-image">
-                  Upload New Image
-                  <input
-                    hidden
-                    type="file"
-                    value={inputValue}
-                    accept="image/png, image/jpeg"
-                    onChange={handleInputImageChange}
-                    id="account-settings-upload-image"
+      <form onSubmit={handleSubmit()}>
+        <DialogTitle
+          id="user-view-edit"
+          sx={{
+            textAlign: 'center',
+            fontSize: '1.5rem !important',
+            px: (theme) => [`${theme.spacing(5)} !important`, `${theme.spacing(10)} !important`],
+            pt: (theme) => [`${theme.spacing(6)} !important`, `${theme.spacing(5)} !important`]
+          }}
+        >
+          Edit User Information
+        </DialogTitle>
+        <DialogContent
+          sx={{
+            pb: (theme) => `${theme.spacing(3)} !important`,
+            px: (theme) => [`${theme.spacing(5)} !important`, `${theme.spacing(8)} !important`]
+          }}
+        >
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={12} sx={{ alignItems: 'center', justifyContent: 'center' }}>
+              <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                <ImgStyled src={imgSrc} alt="Profile Pic" />
+                <div>
+                  <ButtonStyled component="label" variant="contained" htmlFor="account-settings-upload-image">
+                    Upload New Image
+                    <input
+                      hidden
+                      type="file"
+                      value={inputValue}
+                      accept="image/png, image/jpeg"
+                      onChange={handleInputImageChange}
+                      id="account-settings-upload-image"
+                    />
+                  </ButtonStyled>
+                </div>
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Controller
+                name="full_name"
+                control={control}
+                rules={{ required: true }}
+                render={({ field: { value, onChange } }) => (
+                  <TextField
+                    fullWidth
+                    value={value}
+                    label="Full Name"
+                    onChange={onChange}
+                    placeholder="John Doe"
+                    error={Boolean(errors.full_name)}
+                    {...(errors.full_name && { helperText: errors.full_name.message })}
                   />
-                </ButtonStyled>
-              </div>
-            </Box>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              fullWidth
-              label="Full Name"
-              placeholder="John Doe"
-              defaultValue={userData?.name}
-              name="name"
-              onChange={handleInputChange}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              fullWidth
-              label="User Name"
-              placeholder="John Doe"
-              defaultValue={userData?.username}
-              name="username"
-              onChange={handleInputChange}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              fullWidth
-              type="email"
-              label="Official Email"
-              defaultValue={userData?.platform_user?.email}
-              name="email"
-              placeholder="john.doe@gmail.com"
-              onChange={handleInputChange}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              fullWidth
-              label="Contact"
-              placeholder="723-348-2344"
-              defaultValue={`${userData?.platform_user?.mobile}`}
-              onChange={handleInputChange}
-              name="phone"
-            />
-          </Grid>
+                )}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Controller
+                name="user_name"
+                control={control}
+                rules={{ required: true }}
+                render={({ field: { value, onChange } }) => (
+                  <TextField
+                    fullWidth
+                    value={value}
+                    label="User Name"
+                    onChange={onChange}
+                    placeholder="John Doe"
+                    error={Boolean(errors.user_name)}
+                    {...(errors.user_name && { helperText: errors.user_name.message })}
+                  />
+                )}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Controller
+                name="email"
+                control={control}
+                rules={{ required: true }}
+                render={({ field: { value, onChange } }) => (
+                  <TextField
+                    fullWidth
+                    type="email"
+                    label="Email"
+                    value={value}
+                    onChange={onChange}
+                    error={Boolean(errors.email)}
+                    placeholder="johndoe@email.com"
+                    {...(errors.email && { helperText: errors.email.message })}
+                  />
+                )}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Controller
+                name="contact"
+                control={control}
+                rules={{ required: true }}
+                render={({ field: { value, onChange } }) => (
+                  <TextField
+                    fullWidth
+                    type="number"
+                    value={value}
+                    label="Contact"
+                    onChange={onChange}
+                    placeholder="(397) 294-5153"
+                    error={Boolean(errors.contact)}
+                    {...(errors.contact && { helperText: errors.contact.message })}
+                  />
+                )}
+              />
+            </Grid>
 
-          <Grid item xs={12} sm={6}>
-            <TextField
-              fullWidth
-              label="Designation"
-              placeholder="Developer"
-              defaultValue={userData?.platform_user?.designation}
-              onChange={handleInputChange}
-              name="designation"
-            />
+            <Grid item xs={12} sm={6}>
+              <Controller
+                name="designation"
+                control={control}
+                rules={{ required: true }}
+                render={({ field: { value, onChange } }) => (
+                  <TextField
+                    fullWidth
+                    value={value}
+                    label="Designation"
+                    onChange={onChange}
+                    placeholder="Business Development Executive"
+                    error={Boolean(errors.designation)}
+                    {...(errors.designation && { helperText: errors.designation.message })}
+                  />
+                )}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Controller
+                name="status"
+                control={control}
+                rules={{ required: true }}
+                render={({ field: { value, onChange } }) => (
+                  <TextField
+                    select
+                    fullWidth
+                    label="Status"
+                    id="validation-status-select"
+                    error={Boolean(errors.status)}
+                    aria-describedby="validation-status-select"
+                    {...(errors.status && { helperText: errors.status.message })}
+                    SelectProps={{ value: value, onChange: (e) => onChange(e) }}
+                  >
+                    <MenuItem value="Active">Active</MenuItem>
+                    <MenuItem value="Inactive">Inactive</MenuItem>
+                  </TextField>
+                )}
+              />
+            </Grid>
+            <Grid item xs={12} sm={12}>
+              <Controller
+                name="role"
+                control={control}
+                rules={{ required: true }}
+                render={({ field: { value, onChange } }) => (
+                  <TextField
+                    select
+                    fullWidth
+                    value={value}
+                    label="Select Role"
+                    onChange={onChange}
+                    SelectProps={{ value: value, onChange: onChange }}
+                    error={Boolean(errors.role)}
+                    {...(errors.role && { helperText: errors.role.message })}
+                  >
+                    {groups?.map((group, index) => (
+                      <MenuItem key={index} value={group?.role?.id}>
+                        {group?.role?.name}
+                      </MenuItem>
+                    ))}
+                  </TextField>
+                )}
+              />
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField select fullWidth label="Status" defaultValue={userData?.is_active} name="is_active" onChange={handleInputChange}>
-              <MenuItem value="1">Active</MenuItem>
-              <MenuItem value="0">Inactive</MenuItem>
-            </TextField>
-          </Grid>
-          <Grid item xs={12} sm={12}>
-            <TextField select fullWidth label="Role" defaultValue={userData?.id} name="role" onChange={handleInputChange}>
-              {groups?.map((group, index) => (
-                <MenuItem key={index} value={group?.id}>
-                  {group?.name}
-                </MenuItem>
-              ))}
-            </TextField>
-          </Grid>
-        </Grid>
-        {/* </form> */}
-      </DialogContent>
-      <DialogActions
-        sx={{
-          justifyContent: 'center',
-          px: (theme) => [`${theme.spacing(5)} !important`, `${theme.spacing(10)} !important`],
-          pb: (theme) => [`${theme.spacing(5)} !important`, `${theme.spacing(5)} !important`]
-        }}
-      >
-        <Button variant="contained" sx={{ mr: 2 }} onClick={handleEdit}>
-          Submit
-        </Button>
-        <Button variant="tonal" color="secondary" onClick={handleEditClose}>
-          Cancel
-        </Button>
-      </DialogActions>
+        </DialogContent>
+        <DialogActions
+          sx={{
+            justifyContent: 'center',
+            px: (theme) => [`${theme.spacing(5)} !important`, `${theme.spacing(10)} !important`],
+            pb: (theme) => [`${theme.spacing(5)} !important`, `${theme.spacing(5)} !important`]
+          }}
+        >
+          <Button type="submit" variant="contained" sx={{ mr: 2 }}>
+            Submit
+          </Button>
+          <Button variant="tonal" color="secondary" onClick={handleClose}>
+            Cancel
+          </Button>
+        </DialogActions>
+      </form>
     </Dialog>
   );
 };
