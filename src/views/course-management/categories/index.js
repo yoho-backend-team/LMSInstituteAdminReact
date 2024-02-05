eee498854251663bb35ff6c058c12074888fddcd
sanjayyoho <140371314+sanjayyoho@users.noreply.github.com>
2024-02-05 18:28:28 +0530

    sanjay commit

--- src/views/course-management/categories/index.js
+++ src/views/course-management/categories/index.js
@@ -2,55 +2,86 @@ import { Grid } from '@mui/material';
 import CategoryCard from 'features/course-management/categories/components/CategoryCard';
 import CategoryFilter from 'features/course-management/categories/components/CategoryFilterCard';
 import CategoryCardHeader from 'features/course-management/categories/components/CategoryCardHeader';
+// import GroupSkeleton from 'components/cards/Skeleton/GroupSkeleton';
+// import { useSelector } from 'react-redux';
+// import { selectLoading as selectGroupLoading } from 'features/user-management/groups/redux/groupSelectors';
 
-const Categories = () => (
+const dummyCategories = [
+  {
+    id: 1,
+    chipText: 'Active',
+    chipColor: 'success',
+    avatarColor: 'error',
+    title: 'React Js',
+    subtitle: '16 Courses',
+    avatarIcon: 'tabler:currency-dollar',
+    image:
+      'https://media.istockphoto.com/id/1411772543/photo/side-profile-of-african-woman-with-afro-isolated-against-a-white-background-in-a-studio.webp?b=1&s=170667a&w=0&k=20&c=AXoZk6bD-xbU4AQ66k4AKpWBRuDgHufmP4A1_Gn_5zg='
+  },
+  {
+    id: 2,
+    chipText: 'Active',
+    chipColor: 'success',
+    avatarColor: 'error',
+    title: 'Angular Js',
+    subtitle: '18 Courses',
+    avatarIcon: 'tabler:currency-dollar',
+    image:
+      'https://media.istockphoto.com/id/1491507378/photo/side-view-of-a-young-black-woman-standing-whilst-listening-to-music-on-her-headphones-with.webp?b=1&s=170667a&w=0&k=20&c=3vq3A-1ydBG8Sf9KD1iYUrB-yf9f0ZVFbxI0Kzx-c0k='
+  },
+  {
+    id: 3,
+    chipText: 'Active',
+    chipColor: 'success',
+    avatarColor: 'error',
+    title: 'Mango Db',
+    subtitle: '26 Courses',
+    avatarIcon: 'tabler:currency-dollar',
+    image:
+      'https://media.istockphoto.com/id/1471295100/photo/senior-black-woman-wearing-white-glasses.webp?b=1&s=170667a&w=0&k=20&c=GayeA6_-SPI23bJ8uglJFHJ093rwrG47Uq_zEz6zvHE='
+  },
+  {
+    id: 4,
+    chipText: 'Active',
+    chipColor: 'success',
+    avatarColor: 'error',
+    title: 'Html Css',
+    subtitle: '50 Courses',
+    avatarIcon: 'tabler:currency-dollar',
+    image:
+      'https://media.istockphoto.com/id/1386479313/photo/happy-millennial-afro-american-business-woman-posing-isolated-on-white.webp?b=1&s=170667a&w=0&k=20&c=ahypUC_KTc95VOsBkzLFZiCQ0VJwewfrSV43BOrLETM='
+  }
+];
+const Categories = () => {
+  // const groupLoading = useSelector(selectGroupLoading);
+  return (
   <Grid>
-    <Grid item xs={12} sm={12}>
-      <CategoryFilter />
-      <CategoryCardHeader />
-    </Grid>
-    <Grid container spacing={2} className="match-height" sx={{ marginTop: 0 }}>
-      <CategoryCard
-        chipText="Active"
-        chipColor="success"
-        avatarColor="error"
-        title="React Js"
-        subtitle="16 Courses"
-        avatarIcon="tabler:currency-dollar"
-        image="https://media.istockphoto.com/id/1411772543/photo/side-profile-of-african-woman-with-afro-isolated-against-a-white-background-in-a-studio.webp?b=1&s=170667a&w=0&k=20&c=AXoZk6bD-xbU4AQ66k4AKpWBRuDgHufmP4A1_Gn_5zg="
-      />
-
-      <CategoryCard
-        chipText="Active"
-        chipColor="success"
-        avatarColor="error"
-        title="Angular Js"
-        subtitle="18 Courses"
-        avatarIcon="tabler:currency-dollar"
-        image="https://media.istockphoto.com/id/1491507378/photo/side-view-of-a-young-black-woman-standing-whilst-listening-to-music-on-her-headphones-with.webp?b=1&s=170667a&w=0&k=20&c=3vq3A-1ydBG8Sf9KD1iYUrB-yf9f0ZVFbxI0Kzx-c0k="
-      />
-
-      <CategoryCard
-        chipText="Active"
-        chipColor="success"
-        avatarColor="error"
-        title="Mango Db"
-        subtitle="26 Courses"
-        avatarIcon="tabler:currency-dollar"
-        image="https://media.istockphoto.com/id/1471295100/photo/senior-black-woman-wearing-white-glasses.webp?b=1&s=170667a&w=0&k=20&c=GayeA6_-SPI23bJ8uglJFHJ093rwrG47Uq_zEz6zvHE="
-      />
-
-      <CategoryCard
-        chipText="Active"
-        chipColor="success"
-        avatarColor="error"
-        title="Html Css"
-        subtitle="50 Courses"
-        avatarIcon="tabler:currency-dollar"
-        image="https://media.istockphoto.com/id/1386479313/photo/happy-millennial-afro-american-business-woman-posing-isolated-on-white.webp?b=1&s=170667a&w=0&k=20&c=ahypUC_KTc95VOsBkzLFZiCQ0VJwewfrSV43BOrLETM="
-      />
-    </Grid>
+    {/* {groupLoading ? (
+      <GroupSkeleton />
+    ) : ( */}
+      <div>
+        <Grid item xs={12} sm={12}>
+          <CategoryFilter />
+          <CategoryCardHeader />
+        </Grid>
+        <Grid container spacing={2} className="match-height" sx={{ marginTop: 0 }}>
+          {dummyCategories.map((category, index) => (
+            <CategoryCard
+              key={index}
+              chipText={category.chipText}
+              chipColor={category.chipColor}
+              avatarColor={category.avatarColor}
+              title={category.title}
+              subtitle={category.subtitle}
+              avatarIcon={category.avatarIcon}
+              image={category.image}
+            />
+          ))}
+        </Grid>
+      </div>
+    {/* )} */}
   </Grid>
-);
+  )
+};
 
 export default Categories;
--- src/views/course-management/courses/index.js
+++ src/views/course-management/courses/index.js
@@ -6,6 +6,49 @@ import CourseCard from 'features/course-management/courses/components/CourseCard
 import CourseCardHeader from 'features/course-management/courses/components/CourseCardHeader';
 import CourseFilter from 'features/course-management/courses/components/CourseFilterCard';
 
+const courseData = [
+  {
+    price: '$200',
+    chipText: 'Active',
+    chipColor: 'success',
+    personName: 'Leonardo DiCaprio',
+    students: '80+ Modules',
+    coursename: 'React Js Masterclass',
+    image:
+      'https://plus.unsplash.com/premium_photo-1663126346116-f0ccaf232268?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8dGVhY2hpbmd8ZW58MHx8MHx8fDA%3D'
+  },
+  {
+    price: '$150',
+    chipText: 'Active',
+    chipColor: 'success',
+    coursename: 'Angular FrontEnd',
+    personName: 'Johnny Depp',
+    students: '180+ Modules',
+    image:
+      'https://images.unsplash.com/photo-1583468982228-19f19164aee2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHRlYWNoaW5nfGVufDB8fDB8fHww'
+  },
+  {
+    price: '$220',
+    chipText: 'Active',
+    chipColor: 'success',
+    coursename: 'Java Backend',
+    personName: 'Tom Cruise',
+    students: '50+ Modules',
+    image:
+      'https://images.unsplash.com/photo-1605711285791-0219e80e43a3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8dGVhY2hpbmd8ZW58MHx8MHx8fDA%3D'
+  },
+  {
+    price: '$300',
+    chipText: 'Active',
+    chipColor: 'success',
+    coursename: 'React Native Materclass',
+    personName: 'Brad Pitt',
+    students: '100+ Modules',
+    image:
+      'https://plus.unsplash.com/premium_photo-1677966720018-580c48b67bd0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHRlYWNoaW5nfGVufDB8fDB8fHww'
+  }
+];
+
 const Courses = () => (
   <Grid>
     <Grid item xs={12} sm={12}>
@@ -13,42 +56,18 @@ const Courses = () => (
       <CourseCardHeader />
     </Grid>
     <Grid container spacing={2} className="match-height" sx={{ marginTop: 0 }}>
-      <CourseCard
-        price="$200"
-        chipText="Active"
-        chipColor="success"
-        personName="Leonardo DiCaprio"
-        students="80+ Modules"
-        coursename="React Js Masterclass"
-        image="https://plus.unsplash.com/premium_photo-1663126346116-f0ccaf232268?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8dGVhY2hpbmd8ZW58MHx8MHx8fDA%3D"
-      />
-      <CourseCard
-        price="$150"
-        chipText="Active"
-        chipColor="success"
-        coursename="Angular FrontEnd"
-        personName="Johnny Depp"
-        students="180+ Modules"
-        image="https://images.unsplash.com/photo-1583468982228-19f19164aee2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHRlYWNoaW5nfGVufDB8fDB8fHww"
-      />
-      <CourseCard
-        price="$220"
-        chipText="Active"
-        chipColor="success"
-        coursename="Java Backend"
-        personName="Tom Cruise"
-        students="50+ Modules"
-        image="https://images.unsplash.com/photo-1605711285791-0219e80e43a3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8dGVhY2hpbmd8ZW58MHx8MHx8fDA%3D"
-      />
-      <CourseCard
-        price="$300"
-        chipText="Active"
-        chipColor="success"
-        coursename="React Native Materclass"
-        personName="Brad Pitt"
-        students="100+ Modules"
-        image="https://plus.unsplash.com/premium_photo-1677966720018-580c48b67bd0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHRlYWNoaW5nfGVufDB8fDB8fHww"
-      />
+      {courseData.map((course, index) => (
+        <CourseCard
+          key={index}
+          price={course.price}
+          chipText={course.chipText}
+          chipColor={course.chipColor}
+          personName={course.personName}
+          students={course.students}
+          coursename={course.coursename}
+          image={course.image}
+        />
+      ))}
     </Grid>
   </Grid>
 );
