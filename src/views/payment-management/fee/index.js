bbe27086e368e89994216b18f9cd84c0852026d3
Arunbalajiyoho <140370898+Arunbalajiyoho@users.noreply.github.com>
2024-02-05 19:50:59 +0530

    arunbalaji commit

--- src/views/payment-management/fee/index.js
+++ src/views/payment-management/fee/index.js
@@ -1,18 +1,37 @@
 // material-ui
 import { Grid } from '@mui/material';
+import { useState,useEffect } from 'react';
 // project imports
 import FeesTable from 'features/payment-management/fees/components/FeesTable';
-
+import FeesTableSkeleton from 'components/cards/Skeleton/PaymentSkeleton';
 // ==============================|| SAMPLE PAGE ||============================== //
 
 const Fee = () => {
+
+  const [loading, setLoading] = useState(true);
+
+  // Simulate loading delay with useEffect
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
-      <Grid>
-        <Grid spacing={1} className="match-height" sx={{ marginTop: 3 }}>
+     <Grid>
+      <Grid spacing={1} className="match-height" sx={{ marginTop: 3 }}>
+        {loading ? (
+          // If data is still loading, display skeleton
+          <FeesTableSkeleton />
+        ) : (
+          // Once data is loaded, display actual FeesTable component
           <FeesTable />
-        </Grid>
+        )}
       </Grid>
+    </Grid>
     </>
   );
 };
