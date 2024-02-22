da56929040e68e90413668d99da25990283e45f4
sanjayyoho <140371314+sanjayyoho@users.noreply.github.com>
2024-02-22 16:22:25 +0530

    sanjay commit

--- src/features/attandence-management/teaching-staff-attandences/components/TeachingStaffCard.js
+++ src/features/attandence-management/teaching-staff-attandences/components/TeachingStaffCard.js
@@ -7,135 +7,147 @@ import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Avatar from 'components/mui/avatar';
 import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { getAllTeachingStaffAttendances } from '../redux/teachingStaffAttendanceThunks';
+import { selectTeachingStaffAttendances } from '../redux/teachingStaffAttendanceSelectors';
 
 const TeachingStaffCard = () => {
-  const data = [
-    {
-      id: '1',
-      name: 'John',
-      email: 'demo@gmail.com',
-      rating: '3',
-      reviews: '100',
-      img: 'https://th.bing.com/th/id/OIP.IGNf7GuQaCqz_RPq5wCkPgAAAA?rs=1&pid=ImgDetMain',
-      isConnected: '0',
-      Present: '10',
-      Absent: '4',
-      status: 'Active'
-    },
-    {
-      id: '2',
-      name: 'Doe',
-      email: 'demo@gmail.com',
-      rating: '4',
-      reviews: '150',
-      img: 'https://sm.askmen.com/t/askmen_in/article/f/facebook-p/facebook-profile-picture-affects-chances-of-gettin_fr3n.1200.jpg',
-      isConnected: '1',
-      Present: '10',
-      Absent: '4',
-      status: 'Active'
-    },
-    {
-      id: '3',
-      name: 'Alex Fransis',
-      email: 'demo@gmail.com',
-      rating: '5',
-      reviews: '160',
-      img: 'https://i1.wp.com/essentiallypop.com/epop/wp-content/uploads/2017/03/Alex-Francis-1.png?fit=1126%2C1130&ssl=1',
-      isConnected: '0',
-      Present: '10',
-      Absent: '4',
-      status: 'Active'
-    },
-    {
-      id: '4',
-      name: 'Mark Zuckerberg',
-      email: 'demo@gmail.com',
-      rating: '5',
-      reviews: '200',
-      img: 'https://th.bing.com/th/id/OIP.2E3Y6iZbN-18fnzwBpX__wExDM?rs=1&pid=ImgDetMain',
-      isConnected: '1',
-      Present: '10',
-      Absent: '4',
-      status: 'Active'
-    },
-    {
-      id: '4',
-      name: 'Elon Musk',
-      email: 'demo@gmail.com',
-      rating: '5',
-      reviews: '100',
-      img: 'https://i1.wp.com/pagesix.com/wp-content/uploads/sites/3/2016/03/471769884.jpg?quality=90&strip=all&ssl=1',
-      isConnected: '0',
-      Present: '10',
-      Absent: '4',
-      status: 'inactive'
-    },
-    {
-      id: '6',
-      name: 'Jeff Bezos',
-      email: 'demo@gmail.com',
-      rating: '4',
-      reviews: '70',
-      img: 'https://crooksandliars.com/files/primary_image/19/02/gettyimages-1079533948_copy.jpeg',
-      isConnected: '1',
-      Present: '10',
-      Absent: '4',
-      status: 'Active'
-    },
-    {
-      id: '7',
-      name: 'Bill Gates',
-      email: 'demo@gmail.com',
-      rating: '3',
-      reviews: '700',
-      img: 'https://www.rollingstone.com/wp-content/uploads/2021/02/Gates_thumb_clean.jpg',
-      isConnected: '1',
-      Present: '10',
-      Absent: '4',
-      status: 'inactive'
-    },
-    {
-      id: '8',
-      name: 'Larry Page',
-      email: 'demo@gmail.com',
-      rating: '4',
-      reviews: '900',
-      img: 'https://th.bing.com/th/id/R.928fbc6429d99bb33e5d3387d331d994?rik=eiZ1vn1%2bvpr7Zg&riu=http%3a%2f%2fcrowdforthink.com%2fassets%2fuploads%2fstories%2fada9c87f4bb7fc4b55c927ac678a74bb.jpeg&ehk=RPx6fS4SVklzSA%2fQQCWMZ4%2fIUKRbAevxTx0tk3Vh1v0%3d&risl=&pid=ImgRaw&r=0',
-      isConnected: '1',
-      Present: '10',
-      Absent: '4',
-      status: 'inactive'
-    },
-    {
-      id: '9',
-      name: 'Sundar Pichai',
-      email: 'demo@gmail.com',
-      rating: '5',
-      reviews: '1000',
-      img: 'https://images.newindianexpress.com/uploads/user/imagelibrary/2015/8/11/7/original/sunder-pichai-twitter.JPG',
-      isConnected: '1',
-      Present: '10',
-      Absent: '4',
-      status: 'inactive'
-    },
-    {
-      id: '10',
-      name: 'Satya Nadella',
-      email: 'demo@gmail.com',
-      rating: '3.5',
-      reviews: '6600',
-      img: 'https://pawantech12.github.io/timeshop/images/image1.jpg',
-      isConnected: '1',
-      Present: '10',
-      Absent: '4',
-      status: 'Active'
-    }
-  ];
+  // const data = [
+  //   {
+  //     id: '1',
+  //     name: 'John',
+  //     email: 'demo@gmail.com',
+  //     rating: '3',
+  //     reviews: '100',
+  //     img: 'https://th.bing.com/th/id/OIP.IGNf7GuQaCqz_RPq5wCkPgAAAA?rs=1&pid=ImgDetMain',
+  //     isConnected: '0',
+  //     Present: '10',
+  //     Absent: '4',
+  //     status: 'Active'
+  //   },
+  //   {
+  //     id: '2',
+  //     name: 'Doe',
+  //     email: 'demo@gmail.com',
+  //     rating: '4',
+  //     reviews: '150',
+  //     img: 'https://sm.askmen.com/t/askmen_in/article/f/facebook-p/facebook-profile-picture-affects-chances-of-gettin_fr3n.1200.jpg',
+  //     isConnected: '1',
+  //     Present: '10',
+  //     Absent: '4',
+  //     status: 'Active'
+  //   },
+  //   {
+  //     id: '3',
+  //     name: 'Alex Fransis',
+  //     email: 'demo@gmail.com',
+  //     rating: '5',
+  //     reviews: '160',
+  //     img: 'https://i1.wp.com/essentiallypop.com/epop/wp-content/uploads/2017/03/Alex-Francis-1.png?fit=1126%2C1130&ssl=1',
+  //     isConnected: '0',
+  //     Present: '10',
+  //     Absent: '4',
+  //     status: 'Active'
+  //   },
+  //   {
+  //     id: '4',
+  //     name: 'Mark Zuckerberg',
+  //     email: 'demo@gmail.com',
+  //     rating: '5',
+  //     reviews: '200',
+  //     img: 'https://th.bing.com/th/id/OIP.2E3Y6iZbN-18fnzwBpX__wExDM?rs=1&pid=ImgDetMain',
+  //     isConnected: '1',
+  //     Present: '10',
+  //     Absent: '4',
+  //     status: 'Active'
+  //   },
+  //   {
+  //     id: '4',
+  //     name: 'Elon Musk',
+  //     email: 'demo@gmail.com',
+  //     rating: '5',
+  //     reviews: '100',
+  //     img: 'https://i1.wp.com/pagesix.com/wp-content/uploads/sites/3/2016/03/471769884.jpg?quality=90&strip=all&ssl=1',
+  //     isConnected: '0',
+  //     Present: '10',
+  //     Absent: '4',
+  //     status: 'inactive'
+  //   },
+  //   {
+  //     id: '6',
+  //     name: 'Jeff Bezos',
+  //     email: 'demo@gmail.com',
+  //     rating: '4',
+  //     reviews: '70',
+  //     img: 'https://crooksandliars.com/files/primary_image/19/02/gettyimages-1079533948_copy.jpeg',
+  //     isConnected: '1',
+  //     Present: '10',
+  //     Absent: '4',
+  //     status: 'Active'
+  //   },
+  //   {
+  //     id: '7',
+  //     name: 'Bill Gates',
+  //     email: 'demo@gmail.com',
+  //     rating: '3',
+  //     reviews: '700',
+  //     img: 'https://www.rollingstone.com/wp-content/uploads/2021/02/Gates_thumb_clean.jpg',
+  //     isConnected: '1',
+  //     Present: '10',
+  //     Absent: '4',
+  //     status: 'inactive'
+  //   },
+  //   {
+  //     id: '8',
+  //     name: 'Larry Page',
+  //     email: 'demo@gmail.com',
+  //     rating: '4',
+  //     reviews: '900',
+  //     img: 'https://th.bing.com/th/id/R.928fbc6429d99bb33e5d3387d331d994?rik=eiZ1vn1%2bvpr7Zg&riu=http%3a%2f%2fcrowdforthink.com%2fassets%2fuploads%2fstories%2fada9c87f4bb7fc4b55c927ac678a74bb.jpeg&ehk=RPx6fS4SVklzSA%2fQQCWMZ4%2fIUKRbAevxTx0tk3Vh1v0%3d&risl=&pid=ImgRaw&r=0',
+  //     isConnected: '1',
+  //     Present: '10',
+  //     Absent: '4',
+  //     status: 'inactive'
+  //   },
+  //   {
+  //     id: '9',
+  //     name: 'Sundar Pichai',
+  //     email: 'demo@gmail.com',
+  //     rating: '5',
+  //     reviews: '1000',
+  //     img: 'https://images.newindianexpress.com/uploads/user/imagelibrary/2015/8/11/7/original/sunder-pichai-twitter.JPG',
+  //     isConnected: '1',
+  //     Present: '10',
+  //     Absent: '4',
+  //     status: 'inactive'
+  //   },
+  //   {
+  //     id: '10',
+  //     name: 'Satya Nadella',
+  //     email: 'demo@gmail.com',
+  //     rating: '3.5',
+  //     reviews: '6600',
+  //     img: 'https://pawantech12.github.io/timeshop/images/image1.jpg',
+  //     isConnected: '1',
+  //     Present: '10',
+  //     Absent: '4',
+  //     status: 'Active'
+  //   }
+  // ];
+ 
+  const dispatch = useDispatch();
+  const TeachingStaffAttendances = useSelector(selectTeachingStaffAttendances);
+  const selectedBranchId = useSelector((state) => state.auth.selectedBranchId);
+  useEffect(() => {
+    dispatch(getAllTeachingStaffAttendances(selectedBranchId));
+  }, [dispatch, selectedBranchId]);
+
   return (
     <>
       <Grid>
         <Grid container xs={12} spacing={2} mt={2}>
-          {data.map((item, i) => (
+          {TeachingStaffAttendances.map((item, i) => (
             <Grid key={i} item xs={12} sm={6} md={4}>
               <Card sx={{ position: 'relative', p: 1.5 }}>
                 <CardContent sx={{ pt: 2.5 }}>
--- src/views/staff-management/teaching-staffs/index.js
+++ src/views/staff-management/teaching-staffs/index.js
@@ -13,101 +13,10 @@ import Avatar from 'components/mui/avatar';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import TeacherFilter from './TeacherFilterCard';
+import { useDispatch,useSelector  } from 'react-redux';
+import { selectTeachingStaffs } from 'features/staff-management/teaching-staffs/redux/teachingStaffSelectors';
+import { getAllTeachingStaffs } from 'features/staff-management/teaching-staffs/redux/teachingStaffThunks';
 
-const data = [
-  {
-    id: '1',
-    name: 'John',
-    email: 'demo@gmail.com',
-    rating: '3',
-    reviews: '100',
-    img: 'https://th.bing.com/th/id/OIP.IGNf7GuQaCqz_RPq5wCkPgAAAA?rs=1&pid=ImgDetMain',
-    isConnected: '0'
-  },
-  {
-    id: '2',
-    name: 'Doe',
-    email: 'demo@gmail.com',
-    rating: '4',
-    reviews: '150',
-    img: 'https://sm.askmen.com/t/askmen_in/article/f/facebook-p/facebook-profile-picture-affects-chances-of-gettin_fr3n.1200.jpg',
-    isConnected: '1'
-  },
-  {
-    id: '3',
-    name: 'Alex Fransis',
-    email: 'demo@gmail.com',
-    rating: '5',
-    reviews: '160',
-    img: 'https://i1.wp.com/essentiallypop.com/epop/wp-content/uploads/2017/03/Alex-Francis-1.png?fit=1126%2C1130&ssl=1',
-    isConnected: '0'
-  },
-  {
-    id: '4',
-    name: 'Mark Zuckerberg',
-    email: 'demo@gmail.com',
-    rating: '5',
-    reviews: '200',
-    img: 'https://th.bing.com/th/id/OIP.2E3Y6iZbN-18fnzwBpX__wExDM?rs=1&pid=ImgDetMain',
-    isConnected: '1'
-  },
-  {
-    id: '4',
-    name: 'Elon Musk',
-    email: 'demo@gmail.com',
-    rating: '5',
-    reviews: '100',
-    img: 'https://i1.wp.com/pagesix.com/wp-content/uploads/sites/3/2016/03/471769884.jpg?quality=90&strip=all&ssl=1',
-    isConnected: '0'
-  },
-  {
-    id: '6',
-    name: 'Jeff Bezos',
-    email: 'demo@gmail.com',
-    rating: '4',
-    reviews: '70',
-    img: 'https://crooksandliars.com/files/primary_image/19/02/gettyimages-1079533948_copy.jpeg',
-    isConnected: '1'
-  },
-  {
-    id: '7',
-    name: 'Bill Gates',
-    email: 'demo@gmail.com',
-    rating: '3',
-    reviews: '700',
-    img: 'https://www.rollingstone.com/wp-content/uploads/2021/02/Gates_thumb_clean.jpg',
-    isConnected: '1'
-  },
-  {
-    id: '8',
-    name: 'Larry Page',
-    email: 'demo@gmail.com',
-    rating: '4',
-    reviews: '900',
-    img: 'https://th.bing.com/th/id/R.928fbc6429d99bb33e5d3387d331d994?rik=eiZ1vn1%2bvpr7Zg&riu=http%3a%2f%2fcrowdforthink.com%2fassets%2fuploads%2fstories%2fada9c87f4bb7fc4b55c927ac678a74bb.jpeg&ehk=RPx6fS4SVklzSA%2fQQCWMZ4%2fIUKRbAevxTx0tk3Vh1v0%3d&risl=&pid=ImgRaw&r=0',
-    isConnected: '1'
-  },
-  {
-    id: '9',
-    name: 'Sundar Pichai',
-    email: 'demo@gmail.com',
-    rating: '5',
-    reviews: '1000',
-    img: 'https://images.newindianexpress.com/uploads/user/imagelibrary/2015/8/11/7/original/sunder-pichai-twitter.JPG',
-    isConnected: '1'
-  },
-  {
-    id: '10',
-    name: 'Satya Nadella',
-    email: 'demo@gmail.com',
-    rating: '3.5',
-    reviews: '6600',
-    img: 'https://pawantech12.github.io/timeshop/images/image1.jpg',
-    isConnected: '1'
-  }
-];
-
-// ==============================|| SAMPLE PAGE ||============================== //
 const useTimeout = (callback, delay) => {
   useEffect(() => {
     const timeoutId = setTimeout(callback, delay);
@@ -119,13 +28,26 @@ const useTimeout = (callback, delay) => {
 const Teaching = () => {
   const [loading, setLoading] = useState(true);
   const [isDeleteDialogOpen, setDeleteDialogOpen] = useState(false);
-  useTimeout(() => {
-    setLoading(false);
-  }, 1000);
+  const teachingStaffs = useSelector(selectTeachingStaffs);
+  const selectedBranchId = useSelector((state) => state.auth.selectedBranchId);
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    const data ={
+      type:"TeachingStaffs",
+      branch_id:selectedBranchId
+    }
+    dispatch(getAllTeachingStaffs(data));
+  }, [dispatch,selectedBranchId]);
 
   const handleStatusChange = () => {
     setDeleteDialogOpen(true);
   };
+
+  useTimeout(() => {
+    setLoading(false);
+  }, 1000);
+console.log(teachingStaffs);
 
   return (
     <>
@@ -135,7 +57,7 @@ const Teaching = () => {
         <Grid>
           <TeacherFilter />
           <Grid container xs={12} spacing={2} mt={2}>
-            {data.map((item, i) => (
+            {teachingStaffs.map((item, i) => (
               <Grid key={i} item xs={12} sm={6} md={4}>
                 <Card sx={{ position: 'relative' }}>
                   <CardContent sx={{ pt: 3 }}>
