9f0e4308343cfe5157ef9b6170ebb5b149eda3da
Mohammed Thasthakir <mohammed.mt392@gmail.com>
2024-02-23 19:18:57 +0530

    updates notification services

--- src/features/notification-management/all-notifications/redux/allNotificationThunks.js
+++ src/features/notification-management/all-notifications/redux/allNotificationThunks.js
@@ -1,8 +1,8 @@
 // groupThunks.js
-import { getAllAllNotifications as fetchAllAllNotifications } from '../services/allNotificationServices'; // Replace with your service file
+import { getAllNotifications as fetchAllAllNotifications } from '../services/allNotificationServices'; // Replace with your service file
 import { setAllNotifications, setLoading } from './allNotificationSlice';
 
-export const getAllAllNotifications = (selectedBranchId) => async (dispatch) => {
+export const getAllNotifications = (selectedBranchId) => async (dispatch) => {
   try {
     dispatch(setLoading(true));
     const response = await fetchAllAllNotifications(selectedBranchId); // Implement this function in your services
