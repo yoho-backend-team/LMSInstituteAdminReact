f57ce9dee79279bba4dcd840f3e5f54220985b00
sanjayyoho <140371314+sanjayyoho@users.noreply.github.com>
2024-02-24 18:06:00 +0530

    sanjay commit

--- src/views/class-management/offline-class/view-class/index.js
+++ src/views/class-management/offline-class/view-class/index.js
@@ -1,124 +1,517 @@
 // material-ui
-import { ClosedCaption, EmojiPeople, LibraryBooks, People, Public, VideoLibrary } from '@mui/icons-material';
-import { Avatar, Box, Card, CardMedia, Divider, Grid, Typography } from '@mui/material';
+import { Box, CardContent, CardHeader, Typography } from '@mui/material';
+import AvatarGroup from '@mui/material/AvatarGroup';
+import Card from '@mui/material/Card';
+import Grid from '@mui/material/Grid';
+import Tooltip from '@mui/material/Tooltip';
+import { useTheme } from '@mui/material/styles';
+import Avatar from 'components/mui/avatar';
+// ** React Imports
+import { useState } from 'react';
+
+// ** MUI Imports
+import { DataGrid } from '@mui/x-data-grid';
+
+// ** Custom Components
 import CustomChip from 'components/mui/chip';
 
-const ViewOfflineClass = () => (
-  <Card sx={{ p: 3, alignItems: 'center' }}>
-    <Box sx={{ p: 2 }}>
-      <Typography variant="h3" mb={1}>
-        UI/UX Basic Fundamentals
-      </Typography>
-      <Typography variant="h6" mb={2}>
-        <span style={{ fontWeight: '600' }}> Prof:</span> Jessica Moni
-      </Typography>
-    </Box>
-    <Grid container xs={12} spacing={1}>
-      <Grid item xs={12} sm={8} sx={{ p: 2, border: '1px solid #BED1CF', borderRadius: 2 }}>
-        <Card>
-          <CardMedia sx={{ height: '14.5625rem' }} image="https://www.devstringx.com/wp-content/uploads/2022/07/React-2048x1024.jpg" />
-        </Card>
+// ** Utils Import
 
-        <Box sx={{ p: 2 }}>
-          <Typography variant="h4" mb={2}>
-            About This Course
-          </Typography>
-          <Typography variant="p">
-            Learn web design in 1 hour with 25+ simple-to-use rules and guidelines — tons of amazing web design resources included!
-          </Typography>
-        </Box>
-        <Divider />
-        <Grid container sx={{ mt: 1, p: 2 }}>
-          <Grid item xs={12} sm={4} sx={{ alignItems: 'center' }}>
-            <Typography sx={{ alignItems: 'center', display: 'flex', mb: 3 }} gap={1}>
-              <EmojiPeople color="primary" />
-              <Typography>Skill level:</Typography> All Level
+const rows = [
+  {
+    id: 1,
+    avatar: '8.png',
+    full_name: "Korrie O'Crevy",
+    post: 'Nuclear Power Engineer',
+    email: 'kocrevy0@thetimes.co.uk',
+    city: 'Krasnosilka',
+    start_date: '09/23/2016',
+    salary: 23896.35,
+    age: '61',
+    experience: '1 Year',
+    present: 2,
+    absent: 1
+  },
+  {
+    id: 7,
+    avatar: '',
+    full_name: 'Eileen Diehn',
+    post: 'Environmental Specialist',
+    email: 'ediehn6@163.com',
+    city: 'Lampuyang',
+    start_date: '10/15/2017',
+    salary: 18991.67,
+    age: '59',
+    experience: '9 Years',
+    present: 1,
+    absent: 2
+  },
+  {
+    id: 11,
+    avatar: '',
+    full_name: 'De Falloon',
+    post: 'Sales Representative',
+    email: 'dfalloona@ifeng.com',
+    city: 'Colima',
+    start_date: '06/12/2018',
+    salary: 19252.12,
+    age: '30',
+    experience: '0 Year',
+    present: 1,
+    absent: 1
+  },
+  {
+    id: 3,
+    avatar: '7.png',
+    full_name: 'Stella Ganderton',
+    post: 'Operator',
+    email: 'sganderton2@tuttocitta.it',
+    city: 'Golcowa',
+    start_date: '03/24/2018',
+    salary: 13076.28,
+    age: '66',
+    experience: '6 Years',
+    present: 1,
+    absent: 2
+  },
+  {
+    id: 5,
+    avatar: '',
+    full_name: 'Harmonia Nisius',
+    post: 'Senior Cost Accountant',
+    email: 'hnisius4@gnu.org',
+    city: 'Lucan',
+    start_date: '08/25/2017',
+    salary: 10909.52,
+    age: '33',
+    experience: '3 Years',
+    present: 2,
+    absent: 1
+  },
+  {
+    id: 6,
+    avatar: '',
+    full_name: 'Genevra Honeywood',
+    post: 'Geologist',
+    email: 'ghoneywood5@narod.ru',
+    city: 'Maofan',
+    start_date: '06/01/2017',
+    salary: 17803.8,
+    age: '61',
+    experience: '1 Year',
+    present: 1,
+    absent: 2
+  },
+  {
+    id: 4,
+    avatar: '8.png',
+    full_name: 'Dorolice Crossman',
+    post: 'Cost Accountant',
+    email: 'dcrossman3@google.co.jp',
+    city: 'Paquera',
+    start_date: '12/03/2017',
+    salary: 12336.17,
+    age: '22',
+    experience: '2 Years',
+    present: 2,
+    absent: 1
+  },
+  {
+    id: 8,
+    avatar: '7.png',
+    full_name: 'Richardo Aldren',
+    post: 'Senior Sales Associate',
+    email: 'raldren7@mtv.com',
+    city: 'Skoghall',
+    start_date: '11/05/2016',
+    salary: 19230.13,
+    age: '55',
+    experience: '5 Years',
+    absent: 1,
+    present: 2
+  },
+  {
+    id: 9,
+    avatar: '2.png',
+    full_name: 'Allyson Moakler',
+    post: 'Safety Technician',
+    email: 'amoakler8@shareasale.com',
+    city: 'Mogilany',
+    start_date: '12/29/2018',
+    salary: 11677.32,
+    age: '39',
+    experience: '9 Years',
+    present: 1,
+    absent: 2
+  },
+  {
+    id: 10,
+    avatar: '7.png',
+    full_name: 'Merline Penhalewick',
+    post: 'Junior Executive',
+    email: 'mpenhalewick9@php.net',
+    city: 'Kanuma',
+    start_date: '04/19/2019',
+    salary: 15939.52,
+    age: '23',
+    experience: '3 Years',
+    present: 2,
+    absent: 1
+  },
+  {
+    id: 12,
+    avatar: '',
+    full_name: 'Cyrus Gornal',
+    post: 'Senior Sales Associate',
+    email: 'cgornalb@fda.gov',
+    city: 'Boro Utara',
+    start_date: '12/09/2017',
+    salary: 16745.47,
+    age: '22',
+    experience: '2 Years',
+    present: 2,
+    absent: 1
+  },
+  {
+    id: 13,
+    avatar: '',
+    full_name: 'Tallou Balf',
+    post: 'Staff Accountant',
+    email: 'tbalfc@sina.com.cn',
+    city: 'Siliana',
+    start_date: '01/21/2016',
+    salary: 15488.53,
+    age: '36',
+    experience: '6 Years',
+    present: 1,
+    absent: 2
+  },
+  {
+    id: 14,
+    avatar: '',
+    full_name: 'Othilia Extill',
+    post: 'Associate Professor',
+    email: 'oextilld@theatlantic.com',
+    city: 'Brzyska',
+    start_date: '02/01/2016',
+    salary: 18442.34,
+    age: '43',
+    experience: '3 Years',
+    present: 2,
+    absent: 1
+  },
+  {
+    id: 15,
+    avatar: '',
+    full_name: 'Wilmar Bourton',
+    post: 'Administrative Assistant',
+    email: 'wbourtone@sakura.ne.jp',
+    city: 'Bích Động',
+    start_date: '04/25/2018',
+    salary: 13304.45,
+    age: '19',
+    experience: '9 Years',
+    present: 2,
+    absent: 1
+  },
+  {
+    id: 16,
+    avatar: '4.png',
+    full_name: 'Robinson Brazenor',
+    post: 'General Manager',
+    email: 'rbrazenorf@symantec.com',
+    city: 'Gendiwu',
+    start_date: '12/23/2017',
+    salary: 11953.08,
+    age: '66',
+    experience: '6 Years',
+    present: 2,
+    absent: 1
+  },
+  {
+    id: 17,
+    avatar: '',
+    full_name: 'Nadia Bettenson',
+    post: 'Environmental Tech',
+    email: 'nbettensong@joomla.org',
+    city: 'Chabařovice',
+    start_date: '07/11/2018',
+    salary: 20484.44,
+    age: '64',
+    experience: '4 Years',
+    present: 1,
+    absent: 2
+  },
+  {
+    id: 18,
+    avatar: '',
+    full_name: 'Titus Hayne',
+    post: 'Web Designer',
+    email: 'thayneh@kickstarter.com',
+    city: 'Yangon',
+    start_date: '05/25/2019',
+    salary: 16871.48,
+    age: '59',
+    experience: '9 Years',
+    present: 1,
+    absent: 2
+  },
+  {
+    id: 19,
+    avatar: '4.png',
+    full_name: 'Roxie Huck',
+    post: 'Administrative Assistant',
+    email: 'rhucki@ed.gov',
+    city: 'Polýkastro',
+    start_date: '04/04/2019',
+    salary: 19653.56,
+    age: '41',
+    experience: '1 Year',
+    present: 2,
+    absent: 1
+  },
+  {
+    id: 20,
+    avatar: '7.png',
+    full_name: 'Latashia Lewtey',
+    post: 'Actuary',
+    email: 'llewteyj@sun.com',
+    city: 'Hougong',
+    start_date: '08/03/2017',
+    salary: 18303.87,
+    age: '35',
+    experience: '5 Years',
+    present: 1,
+    absent: 1
+  },
+];
+
+const present = {
+  1: { title: 'Present', color: 'success' },
+  2: { title: 'Absent', color: 'error' },
+};
+
+const absent = {
+  1: { title: 'Present', color: 'success' },
+  2: { title: 'Absent', color: 'error' },
+};
+const ViewOfflineClass = () => {
+  const theme = useTheme();
+
+  const [paginationModel, setPaginationModel] = useState({ page: 0, pageSize: 7 });
+
+  const columns = [
+    {
+      flex: 0.2,
+      minWidth: 120,
+      headerName: 'Student ID',
+      field: 'start_date',
+      renderCell: (params) => (
+        <Typography variant="body2" sx={{ color: 'text.primary' }}>
+          {params.row.id}
+        </Typography>
+      )
+    },
+    {
+      flex: 0.275,
+      minWidth: 290,
+      field: 'full_name',
+      headerName: 'Student Name',
+      renderCell: (params) => {
+        const { row } = params;
+        return (
+          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+            <Typography noWrap variant="body2" sx={{ color: 'text.primary', fontWeight: 600 }}>
+              {row.full_name}
             </Typography>
-            <Typography sx={{ alignItems: 'center', display: 'flex', mb: 3 }} gap={1}>
-              <People color="error" />
-              <Typography> Students:</Typography> 38815
-            </Typography>
-            <Typography sx={{ alignItems: 'center', display: 'flex', mb: 3 }} gap={1}>
-              <Public color="success" />
-              <Typography>Languages: </Typography> English
-            </Typography>
-          </Grid>
-          <Grid item xs={12} sm={4}>
-            <Typography sx={{ alignItems: 'center', display: 'flex', mb: 3 }} gap={1}>
-              <ClosedCaption color="primary" />
-              <Typography> Captions:</Typography> true
-            </Typography>
-            <Typography sx={{ alignItems: 'center', display: 'flex', mb: 3 }} gap={1}>
-              <LibraryBooks color="secondary" />
-              <Typography> Lectures:</Typography> 19
-            </Typography>
-            <Typography sx={{ alignItems: 'center', display: 'flex', mb: 3 }} gap={1}>
-              <VideoLibrary color="error" />
-              <Typography>Video:</Typography> 1.5 total hours
-            </Typography>
-          </Grid>
+          </Box>
+        );
+      }
+    },
+    {
+      flex: 0.2,
+      minWidth: 140,
+      field: 'present',
+      headerName: 'Present',
+
+      renderCell: (params) => {
+        const presentStatus = present[params.row.present]; 
+
+        return (
+          <CustomChip
+          rounded
+          size="small"
+          skin="light"
+          color={presentStatus.color} 
+          label={presentStatus.title} 
+          sx={{ '& .MuiChip-label': { textTransform: 'capitalize' } }}
+          />
+        );
+      }
+    },
+    {
+      flex: 0.2,
+      minWidth: 140,
+      field: 'absent',
+      headerName: 'Absent',
+
+      renderCell: (params) => {
+        const absentStatus = absent[params.row.absent];
+
+        return (
+          <CustomChip
+            rounded
+            size="small"
+            skin="light"
+            color={absentStatus.color}
+            label={absentStatus.title}
+            sx={{ '& .MuiChip-label': { textTransform: 'capitalize' } }}
+          />
+        );
+      }
+    }
+  ];
+
+  return (
+    <Box>
+      <Grid container>
+        <Grid item xs={12}>
+          <Card>
+            <CardHeader title="Ux Class 13402324" />
+            <CardContent sx={{ mt: 0, pt: 0 }}>
+              <Grid container spacing={4}>
+                <Grid item>
+                  <Typography variant="h5" sx={{ color: 'grey.500' }}>
+                    Course
+                  </Typography>
+                  <Typography variant="h4" sx={{ mt: 1 }}>
+                    UX Design
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography variant="h5" sx={{ color: 'grey.500' }}>
+                    Batch
+                  </Typography>
+                  <Typography variant="h4" sx={{ mt: 1 }}>
+                    #2323251
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography variant="h5" sx={{ color: 'grey.500' }}>
+                    Duration
+                  </Typography>
+                  <Typography variant="h4" sx={{ mt: 1 }}>
+                    1:24:36
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography variant="h5" sx={{ color: 'grey.500' }}>
+                    Date
+                  </Typography>
+                  <Typography variant="h4" sx={{ mt: 1 }}>
+                    13 Feb 2024
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography variant="h5" sx={{ color: 'grey.500' }}>
+                    Sarted At
+                  </Typography>
+                  <Typography variant="h4" sx={{ mt: 1 }}>
+                    10:30 AM
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography variant="h5" sx={{ color: 'grey.500' }}>
+                    Ended At
+                  </Typography>
+                  <Typography variant="h4" sx={{ mt: 1 }}>
+                    11:54 AM
+                  </Typography>
+                </Grid>
+              </Grid>
+            </CardContent>
+            <CardContent sx={{ mt: 0, pt: 0 }}>
+              <Grid container spacing={4}>
+                <Grid item>
+                  <Typography variant="h5" sx={{ color: 'grey.500' }}>
+                    Instructor
+                  </Typography>
+                  <Box sx={{ display: 'flex', alignItems: 'center', mt: 1 }}>
+                    <Box>
+                      <AvatarGroup className="pull-up" sx={{ display: 'flex', alignItems: 'center' }}>
+                        <Tooltip title="Olivia Sparks">
+                          <Avatar src="/images/avatars/4.png" alt="Olivia Sparks" sx={{ width: 25, height: 25 }} />
+                        </Tooltip>
+                        <Tooltip title="Howard Lloyd">
+                          <Avatar src="/images/avatars/5.png" alt="Howard Lloyd" sx={{ width: 25, height: 25 }} />
+                        </Tooltip>
+                        <Tooltip title="Hallie Richards">
+                          <Avatar src="/images/avatars/6.png" alt="Hallie Richards" sx={{ width: 25, height: 25 }} />
+                        </Tooltip>
+                        <Tooltip title="Alice Cobb">
+                          <Avatar src="/images/avatars/8.png" alt="Alice Cobb" sx={{ width: 25, height: 25 }} />
+                        </Tooltip>
+                      </AvatarGroup>
+                    </Box>
+                    <Box>
+                      <Typography variant="h4">Jerome Bell</Typography>
+                    </Box>
+                  </Box>
+                </Grid>
+                <Grid item>
+                  <Typography variant="h5" sx={{ color: 'grey.500' }}>
+                    Coordinator
+                  </Typography>
+                  <Box sx={{ display: 'flex', alignItems: 'center', mt: 1 }}>
+                    <Box>
+                      <AvatarGroup className="pull-up" sx={{ display: 'flex', alignItems: 'center' }}>
+                        <Tooltip title="Olivia Sparks">
+                          <Avatar src="/images/avatars/4.png" alt="Olivia Sparks" sx={{ width: 25, height: 25 }} />
+                        </Tooltip>
+                        <Tooltip title="Howard Lloyd">
+                          <Avatar src="/images/avatars/5.png" alt="Howard Lloyd" sx={{ width: 25, height: 25 }} />
+                        </Tooltip>
+                        <Tooltip title="Hallie Richards">
+                          <Avatar src="/images/avatars/6.png" alt="Hallie Richards" sx={{ width: 25, height: 25 }} />
+                        </Tooltip>
+                        <Tooltip title="Alice Cobb">
+                          <Avatar src="/images/avatars/8.png" alt="Alice Cobb" sx={{ width: 25, height: 25 }} />
+                        </Tooltip>
+                      </AvatarGroup>
+                    </Box>
+                    <Box>
+                      <Typography variant="h4">Robert Fox</Typography>
+                    </Box>
+                  </Box>
+                </Grid>
+                <Grid item>
+                  <Typography variant="h5" sx={{ color: 'grey.500' }}>
+                    Class Type
+                  </Typography>
+                  <Box sx={{ mt: 1.5, display: 'flex', alignItems: 'center' }}>
+                    <Typography variant="h4">Online</Typography>
+                    <Typography variant="h5" sx={{ color: theme.palette.primary.main, ml: 1 }}>
+                      Visit Previous Class
+                    </Typography>
+                  </Box>
+                </Grid>
+              </Grid>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} mt={3}>
+          <DataGrid
+            autoHeight
+            rows={rows}
+            columns={columns}
+            pageSizeOptions={[7, 10, 25, 50]}
+            paginationModel={paginationModel}
+            onPaginationModelChange={setPaginationModel}
+          />
         </Grid>
       </Grid>
-      <Grid item xs={12} sm={4}>
-        <Card sx={{ p: 2, mb: 3 }}>
-          <Typography variant="h5" mb={2}>
-            Instructor
-          </Typography>
-          <Grid container>
-            <Grid item sx={12} sm={12}>
-              <Box display="flex" alignItems="center" gap={3} sx={{ borderRadius: '10px', overflow: 'hidden' }}>
-                <Avatar alt="Remy Sharp" src="https://d2qp0siotla746.cloudfront.net/img/use-cases/profile-picture/template_0.jpg" />
-                <Typography variant="h4">Devonne Wallbridge</Typography>
-              </Box>
-            </Grid>
-            <Grid item xs={12} sm={3}></Grid>
-            <Grid item xs={12} sm={9}>
-              <Typography variant="h6" sx={{ fontWeight: 600 }}>
-                Web Developer, Designer, and Teacher
-              </Typography>
-            </Grid>
-          </Grid>
-        </Card>
-        <Card sx={{ p: 2, mb: 3 }}>
-          <Typography variant="h5" mb={2}>
-            Teacher
-          </Typography>
-          <Grid container>
-            <Grid item sx={12} sm={12}>
-              <Box display="flex" alignItems="center" gap={3} sx={{ borderRadius: '10rem', overflow: 'hidden' }}>
-                <Avatar alt="Remy Sharp" src="https://i.pinimg.com/originals/14/3e/65/143e656d09e4848f85289b6a602d75ec.jpg" />
-                <Typography variant="h4">Jessica Moni</Typography>
-              </Box>
-            </Grid>
-            <Grid item xs={12} sm={3}></Grid>
-            <Grid item xs={12} sm={9}>
-              <Typography variant="h6" sx={{ fontWeight: 600 }}>
-                Web Developer, Designer, and Teacher
-              </Typography>
-            </Grid>
-          </Grid>
-        </Card>
-        <Card sx={{ p: 2 }}>
-          <Box>
-            <Typography variant="h5" mb={2}>
-              Other Details
-            </Typography>
-            <Divider />
-            <Typography mt={2} mb={2}>
-              Duration: 3:00 hrs
-            </Typography>
-            <Typography mb={2}>Date: 09/02/1999</Typography>
-            <Typography mb={2}>Language: English</Typography>
-            <Typography mb={4}>Level: Beginner</Typography>
-          </Box>
-          <Box alignItems="center" display="flex" gap={2}>
-            <CustomChip label="React" skin="light" color="primary" />
-            <CustomChip label="material UI" skin="light" color="success" />
-          </Box>
-        </Card>
-      </Grid>
-    </Grid>
-  </Card>
-);
+    </Box>
+  );
+};
 
 export default ViewOfflineClass;
