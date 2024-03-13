d57868582f0fe34728dae0097a88af60a88d8b1b
Mohammed Thasthakir <mohammed.mt392@gmail.com>
2024-03-13 16:13:59 +0530

    thasthakir commit

--- src/features/class-management/offline-classes/redux/offlineClassThunks.js
+++ src/features/class-management/offline-classes/redux/offlineClassThunks.js
@@ -2,10 +2,10 @@
 import { getAllOfflineClasses as fetchAllOfflineClasses } from '../services/offlineClassServices'; // Replace with your service file
 import { setOfflineClasses, setLoading } from './offlineClassSlice';
 
-export const getAllOfflineClasses = (selectedBranchId) => async (dispatch) => {
+export const getAllOfflineClasses = (data) => async (dispatch) => {
   try {
     dispatch(setLoading(true));
-    const response = await fetchAllOfflineClasses(selectedBranchId); // Implement this function in your services
+    const response = await fetchAllOfflineClasses(data); // Implement this function in your services
     dispatch(setOfflineClasses(response?.data?.data));
   } catch (error) {
     console.error(error);
