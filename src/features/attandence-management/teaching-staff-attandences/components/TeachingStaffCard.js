4822cc2691984e3c8e84cae507f7fd05a2aee92e
Arunbalajiyoho <140370898+Arunbalajiyoho@users.noreply.github.com>
2024-04-10 10:20:06 +0530

    arunbalaji commit

--- src/features/attandence-management/teaching-staff-attandences/components/TeachingStaffCard.js
+++ src/features/attandence-management/teaching-staff-attandences/components/TeachingStaffCard.js
@@ -6,6 +6,7 @@ import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Avatar from 'components/mui/avatar';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 const TeachingStaffCard = ({ teachingStaffs }) => {
   return (
