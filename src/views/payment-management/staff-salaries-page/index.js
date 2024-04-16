1cb4be3e9cc039027a3e75484d4f5ae2edd200ac
Arunbalajiyoho <140370898+Arunbalajiyoho@users.noreply.github.com>
2024-04-16 18:31:56 +0530

    arunbalaji commit

--- src/views/payment-management/staff-salaries-page/index.js
+++ src/views/payment-management/staff-salaries-page/index.js
@@ -2,12 +2,11 @@ import { Grid } from '@mui/material';
 import SalaryTable from 'features/payment-management/salaries/components/SalaryTable';
 
 const Salary = () => {
-
   return (
     <>
-      <Grid>
-        <Grid container spacing={1} className="match-height">
-           <SalaryTable />
+      <Grid container spacing={1} className="match-height">
+        <Grid item xs={12} sm={12}>
+          <SalaryTable />
         </Grid>
       </Grid>
     </>
