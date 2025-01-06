70f29559b6be228d17918fff27f5b2eda780c4b1
Anbarasan-yoho <anbarasan.yoho@gmail.com>
2025-01-06 22:41:36 +0530

    update in Profile UI(UserViewLeft and UserEditDialog)

--- src/features/account-settings/components/UserEditDialog.js
+++ src/features/account-settings/components/UserEditDialog.js
@@ -95,15 +95,15 @@ const UserEditDialog = ({ openEdit, handleEditClose, userData, setRefetch }) => 
   const [selectedImage, setSelectedImage] = useState('');
   const [imgSrc, setImgSrc] = useState(image);
   const [groups, setGroups] = useState([]);
-  const { show, hide } = useSpinner()
+  const { show, hide } = useSpinner();
 
   useEffect(() => {
     getGroups();
   }, []);
-  console.log(userData,"userData")
+  console.log(userData, 'userData');
   const getGroups = async () => {
     try {
-      const result = await getAllGroups({institute_id:useInstitute().getInstituteId()});
+      const result = await getAllGroups({ institute_id: useInstitute().getInstituteId() });
       if (result.success) {
         setGroups(result.data);
       } else {
@@ -117,8 +117,7 @@ const UserEditDialog = ({ openEdit, handleEditClose, userData, setRefetch }) => 
   const ImgStyled = styled('img')(({ theme }) => ({
     width: 100,
     height: 100,
-    marginRight: theme.spacing(2),
-    borderRadius: theme.shape.borderRadius
+    borderRadius: 50
   }));
 
   const ButtonStyled = styled(Button)(({ theme }) => ({
@@ -130,16 +129,14 @@ const UserEditDialog = ({ openEdit, handleEditClose, userData, setRefetch }) => 
 
   const handleInputImageChange = async (file) => {
     try {
-     show()
-     const { files } = file.target
-     const form_data = new FormData()
-     form_data.append("file",files)
-     const response = await client.file.upload(form_data)
-     
+      show();
+      const { files } = file.target;
+      const form_data = new FormData();
+      form_data.append('file', files);
+      const response = await client.file.upload(form_data);
     } catch (error) {
-      
-    }finally{
-      hide()
+    } finally {
+      hide();
     }
     const reader = new FileReader();
     const { files } = file.target;
@@ -152,7 +149,6 @@ const UserEditDialog = ({ openEdit, handleEditClose, userData, setRefetch }) => 
       }
     }
   };
-
 
   const onSubmit = async (data) => {
     const InputData = new FormData();
@@ -204,7 +200,44 @@ const UserEditDialog = ({ openEdit, handleEditClose, userData, setRefetch }) => 
         >
           <Grid container spacing={3}>
             <Grid item xs={12} sm={12} sx={{ alignItems: 'center', justifyContent: 'center' }}>
-              <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+              <Box sx={{ position: 'relative', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                {!selectedImage && (
+                  <ImgStyled src={userData?.image ? getImageUrl(userData?.image) : profilePlaceholder} alt="Profile Pic" />
+                )}
+
+                {selectedImage && <ImgStyled src={userData?.image ? getImageUrl(userData?.image) : profilePlaceholder} alt="Profile Pic" />}
+
+                {/* Button placed in the bottom-right of the image */}
+                <ButtonStyled
+                  component="label"
+                  variant="contained"
+                  htmlFor="account-settings-upload-image"
+                  sx={{
+                    position: 'absolute',
+                    bottom: '8px',
+                    right: '240px',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    borderRadius: 50
+                  }}
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 96 960 960" width="20" fill="#fff">
+                    <path d="M450 856h60V596l98 98 43-43-161-161-161 161 43 43 98-98v260ZM240 936q-33 0-56.5-23.5T160 856V296q0-33 23.5-56.5T240 216h480q33 0 56.5 23.5T800 296v560q0 33-23.5 56.5T720 936H240Zm0-80h480V296H240v560ZM240 296v560-560Z" />
+                  </svg>
+
+                  <input
+                    hidden
+                    type="file"
+                    value={inputValue}
+                    accept="image/png, image/jpeg"
+                    onChange={handleInputImageChange}
+                    id="account-settings-upload-image"
+                  />
+                </ButtonStyled>
+              </Box>
+
+              {/* <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                 {!selectedImage && (
                   <ImgStyled
                     src={userData?.image ? getImageUrl(userData?.image):profilePlaceholder}
@@ -226,7 +259,7 @@ const UserEditDialog = ({ openEdit, handleEditClose, userData, setRefetch }) => 
                     />
                   </ButtonStyled>
                 </div>
-              </Box>
+              </Box> */}
             </Grid>
             <Grid item xs={12} sm={6}>
               <Controller
--- src/features/account-settings/components/view/UserViewLeft.js
+++ src/features/account-settings/components/view/UserViewLeft.js
@@ -25,7 +25,7 @@ const UserViewLeft = ({ userData, id, setRefetch }) => {
     0: 'error'
   };
 
-  
+
   // ** States
   const [openEdit, setOpenEdit] = useState(false);
 
@@ -37,30 +37,72 @@ const UserViewLeft = ({ userData, id, setRefetch }) => {
     <Grid container spacing={3} justifyContent="center">
       <Grid item xs={12} >
         <Card sx={{ boxShadow: 3, borderRadius: 2 }}>
-          <CardContent
-            sx={{
-              pt: 4,
-              display: 'flex',
-              alignItems: 'center',
-              flexDirection: 'column',
-              backgroundColor: 'primary.light',
-              color: 'primary.contrastText'
-            }}
-          >
-            <Avatar
-              src={userData?.image ? getImageUrl(userData.image) : profilePlaceholder}
-              alt={userData?.name}
-              sx={{ width: 120, height: 120, mb: 3, border: '4px solid', borderColor: 'primary.main' }}
-            />
-            <Typography variant="h5" sx={{ fontWeight: 'bold', mb: 1 }}>
-              {`${userData?.first_name || ''} ${userData?.last_name || ''}`}
-            </Typography>
-            <Chip
-              label={userData?.role?.identity || 'User'}
-              color="warning"
-              sx={{ textTransform: 'capitalize', fontWeight: 'bold' }}
-            />
-          </CardContent>
+        <CardContent
+  sx={{
+    pt: 4,
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
+    backgroundColor: 'primary.light',
+    color: 'primary.contrastText',
+    position: 'relative', // Added for absolute positioning inside this CardContent
+  }}
+>
+  <Avatar
+    src={userData?.image ? getImageUrl(userData.image) : profilePlaceholder}
+    alt={userData?.name}
+    sx={{ width: 120, height: 120, mb: 3, border: '4px solid', borderColor: 'primary.main' }}
+  />
+  <Box
+    sx={{
+      position: 'absolute',
+      bottom: '110px',
+      right: 'calc(50% - 60px)', // Adjust this value based on avatar's alignment
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}
+  >
+    <Button
+      onClick={handleEditClickOpen}
+      sx={{
+        backgroundColor: 'primary.main',
+        color: 'success',
+        minWidth: 0,
+        padding: '6px',
+        borderRadius: '50%',
+        '&:hover': {
+          backgroundColor: '#0aa865',
+        },
+      }}
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="white" d="m12.25 10.825l.925.925L18.6 6.325l-.925-.925zM5 19h.925l5.825-5.825l-.925-.925L5 18.075zm8.875-5.125l-3.75-3.75L14.3 5.95l-.725-.725L8.8 10q-.3.3-.7.3t-.7-.3t-.3-.712t.3-.713l4.75-4.75q.6-.6 1.413-.6t1.412.6l.725.725l1.25-1.25q.3-.3.713-.3t.712.3L20.7 5.625q.3.3.3.712t-.3.713zM4 21q-.425 0-.712-.288T3 20v-1.925q0-.4.15-.763t.425-.637l6.55-6.55l3.75 3.75l-6.55 6.55q-.275.275-.638.425t-.762.15z"/></svg>
+      {/* <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        strokeWidth={2}
+        width={20}
+        height={20}
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M15.232 5.232l3.536 3.536m-2.036-1.036L10.5 13.5a2.121 2.121 0 01-.797.464l-2.121.707.707-2.121a2.121 2.121 0 01.464-.797l5.657-5.657m2.828 0a1.5 1.5 0 112.121 2.121L16.5 8.5m0 0L14.5 10.5m0 0L8.5 16.5a1 1 0 00-.293.707V18.5h1.293c.189 0 .374-.074.514-.214l6-6z"
+        />
+      </svg> */}
+    </Button>
+  </Box>
+  <Typography variant="h2" sx={{ fontWeight: 'bold', mb: 1 }}>
+    {`${userData?.first_name || ''} ${userData?.last_name || ''}`}
+  </Typography>
+  <Chip
+    label={userData?.role?.identity || 'User'}
+    color="warning"
+    sx={{ textTransform: 'capitalize', fontWeight: 'bold' }}
+  />
+</CardContent>
 
           <Divider />
 
@@ -68,9 +110,9 @@ const UserViewLeft = ({ userData, id, setRefetch }) => {
             <Typography variant="subtitle2" color="text.secondary" sx={{ mb: 3, textTransform: 'uppercase', fontWeight: 500 }}>
               User Details
             </Typography>
-            <Box sx={{ display: 'grid', rowGap: 1 }}>
+            <Box sx={{ display: 'grid', rowGap: 2 }}>
               <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                <Typography color="text.primary" fontWeight={500}>
+                <Typography color="text.primary"  fontWeight={500}>
                   Name:
                 </Typography>
                 <Typography>{`${userData?.first_name || ''} ${userData?.last_name || ''}`}</Typography>
@@ -102,17 +144,6 @@ const UserViewLeft = ({ userData, id, setRefetch }) => {
               </Box>
             </Box>
           </CardContent>
-
-          <CardActions sx={{ display: 'flex', justifyContent: 'center', p: 2 }}>
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={handleEditClickOpen}
-              sx={{ px: 4, py: 1, textTransform: 'none' ,borderRadius: '50px' }}
-            >
-              Edit Details
-            </Button>
-          </CardActions>
         </Card>
         <UserEditDialog id={id} userData={userData} openEdit={openEdit} handleEditClose={handleEditClose} setRefetch={setRefetch} />
       </Grid>
