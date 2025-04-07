f8ce8a54d078a216976d598bfaaaed89222a2e1d
Sumi-Yoho <sumiofficialyoho@gmail.com>
2025-04-07 18:32:27 +0530

    Placement Overview page completed

--- src/views/placement-management/placement/placement.js
+++ src/views/placement-management/placement/placement.js
@@ -1,12 +1,115 @@
-import { Grid } from '@mui/material';
-import AddPlacement from 'features/placement/add-placement';
-import PalacementHeader from 'features/placement/placement-header.js'
+import { Grid, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Box, Icon } from '@mui/material';
+import client from 'api/client';
+import PlacementHeader from 'features/placement/placement-header';
+import { useEffect, useState } from 'react';
+import { useInstitute } from 'utils/get-institute-details';
+import NotificationSkeleton from 'components/cards/Skeleton/NotificationSkeleton';
+import { getImageUrl } from 'utils/imageUtils';
+import Avatar from '@mui/material/Avatar';
+import AvatarGroup from '@mui/material/AvatarGroup';
+import OptionsMenu from 'components/option-menu';
+import { Link } from 'react-router-dom';
 const Placement = () => {
+  const [placements, setPlacements] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const instituteId = useInstitute().getDetails();
+
+  useEffect(() => {
+    const getAllPlacements = async () => {
+      try {
+        const res = await client.placements.getAll({ institute_id: instituteId._id });
+        setPlacements(res.data);
+      } catch (err) {
+        console.error('Failed to fetch placements', err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getAllPlacements();
+  }, []);
+
   return (
     <>
       <Grid container spacing={1} className="match-height">
-        <Grid item xs={12} sm={12}>
-          <PalacementHeader/>
+        <Grid item xs={12}>
+          <PlacementHeader />
+        </Grid>
+
+        <Grid item xs={12}>
+          {loading ? (
+            <NotificationSkeleton />
+          ) : (
+            <TableContainer component={Paper}>
+              <Table>
+                <TableHead>
+                  <TableRow sx={{ backgroundColor: '#F6F6F7' }}>
+                    <TableCell>Company Name</TableCell>
+                    <TableCell>Interview Date</TableCell>
+                    <TableCell>Job name</TableCell>
+                    <TableCell>Students</TableCell>
+                    <TableCell>Active</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {placements.map((placement, index) => (
+                    <TableRow key={index}>
+                      <TableCell>
+                        <div>
+                          <div style={{ fontWeight: 'bold' }}>{placement.company.name}</div>
+                          <div style={{ fontSize: '0.875rem', color: '#555' }}>{placement.company.email}</div>
+                        </div>
+                      </TableCell>{' '}
+                      <TableCell>
+                        {new Date(placement.schedule.interviewDate).toLocaleDateString('en-IN', {
+                          year: 'numeric',
+                          month: 'short',
+                          day: 'numeric'
+                        })}
+                      </TableCell>
+                      <TableCell>{placement.job.name}</TableCell>
+                      <TableCell>
+                        <AvatarGroup spacing="small" total={placement.student.length > 3 ? placement.student.length : undefined}>
+                          {placement.student.slice(0, placement.student.length > 3 ? 3 : placement.student.length).map((student, i) => (
+                            <Avatar key={i} src={getImageUrl(student.image)} />
+                          ))}
+                        </AvatarGroup>
+                      </TableCell>
+                      <TableCell>
+                        <div>
+                          {' '}
+                          <Box>
+                            <OptionsMenu
+                              menuProps={{ sx: { '& .MuiMenuItem-root svg': { mr: 2 } } }}
+                              iconButtonProps={{ size: 'small', sx: { color: 'text.secondary' } }}
+                              options={[
+                                {
+                                  text: 'Edit',
+                                  icon: <Icon icon="tabler:edit" />,
+                                  menuItemProps: {
+                                    onClick: () => {
+                                      handleEdit();
+                                    }
+                                  }
+                                },
+                                {
+                                  text: 'Delete',
+                                  icon: <Icon icon="mdi:delete-outline" />,
+                                  menuItemProps: {
+                                    // onClick: () => handleDelete(card?.uuid)
+                                  }
+                                }
+                              ]}
+                            />
+                          </Box>
+                        </div>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          )}
         </Grid>
       </Grid>
     </>
